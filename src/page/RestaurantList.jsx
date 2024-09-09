3ca5f695f9babdbc8ff76729733b86452930b956
salendra <salendra@uxdlabtech.com>
2024-09-09 13:43:01 +0530

    done

--- src/page/RestaurantList.jsx
+++ src/page/RestaurantList.jsx
@@ -1,63 +1,54 @@
 import React from 'react'
 import './Restaurantlist.css'
-
+import { Link } from 'react-router-dom'
 
 const RestaurantList = () => {
   return (
     <div className='container'>
         <h3>Restaurant list</h3>
         <div className='restaurantlist'>
-        <div className="card onecard" >
+        <Link to="/restaurantview"><div className="card onecard" >
             <img style={{borderRadius:'20px'}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/h7oibtpm7kqks8cqz3gh" alt="image" />
             <div className="card-body">
                 <h5 className="card-title">Waaw Momo</h5>
                 <a href="#" className="btn btn-primary">Explore</a>
             </div>
-        </div>
+        </div></Link>
+        <Link to="/restaurantview"><div className="card onecard" >
+            <img style={{borderRadius:'20px'}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/h7oibtpm7kqks8cqz3gh" alt="image" />
+            <div className="card-body">
+                <h5 className="card-title">Waaw Momo</h5>
+                <a href="#" className="btn btn-primary">Explore</a>
+            </div>
+        </div></Link>
+        <Link to="/restaurantview"><div className="card onecard" >
+            <img style={{borderRadius:'20px'}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/h7oibtpm7kqks8cqz3gh" alt="image" />
+            <div className="card-body">
+                <h5 className="card-title">Waaw Momo</h5>
+                <a href="#" className="btn btn-primary">Explore</a>
+            </div>
+        </div></Link>
+        <Link to="/restaurantview"><div className="card onecard" >
+            <img style={{borderRadius:'20px'}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/h7oibtpm7kqks8cqz3gh" alt="image" />
+            <div className="card-body">
+                <h5 className="card-title">Waaw Momo</h5>
+                <a href="#" className="btn btn-primary">Explore</a>
+            </div>
+        </div></Link>
+        <Link to="/restaurantview"><div className="card onecard" >
+            <img style={{borderRadius:'20px'}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/h7oibtpm7kqks8cqz3gh" alt="image" />
+            <div className="card-body">
+                <h5 className="card-title">Waaw Momo</h5>
+                <a href="#" className="btn btn-primary">Explore</a>
+            </div>
+        </div></Link><Link to="/restaurantview"><div className="card onecard" >
+            <img style={{borderRadius:'20px'}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/h7oibtpm7kqks8cqz3gh" alt="image" />
+            <div className="card-body">
+                <h5 className="card-title">Waaw Momo</h5>
+                <a href="#" className="btn btn-primary">Explore</a>
+            </div>
+        </div></Link>
 
-        <div className="card onecard" >
-            <img style={{borderRadius:'20px'}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/h7oibtpm7kqks8cqz3gh" alt="image" />
-            <div className="card-body">
-                <h5 className="card-title">Waaw Momo</h5>
-                <a href="#" className="btn btn-primary">Explore</a>
-            </div>
-        </div>
-        <div className="card onecard" >
-            <img style={{borderRadius:'20px'}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/h7oibtpm7kqks8cqz3gh" alt="image" />
-            <div className="card-body">
-                <h5 className="card-title">Waaw Momo</h5>
-                <a href="#" className="btn btn-primary">Explore</a>
-            </div>
-        </div>
-
-        <div className="card onecard" >
-            <img style={{borderRadius:'20px'}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/h7oibtpm7kqks8cqz3gh" alt="image" />
-            <div className="card-body">
-                <h5 className="card-title">Waaw Momo</h5>
-                <a href="#" className="btn btn-primary">Explore</a>
-            </div>
-        </div>
-        <div className="card onecard" >
-            <img style={{borderRadius:'20px'}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/h7oibtpm7kqks8cqz3gh" alt="image" />
-            <div className="card-body">
-                <h5 className="card-title">Waaw Momo</h5>
-                <a href="#" className="btn btn-primary">Explore</a>
-            </div>
-        </div>
-        <div className="card onecard" >
-            <img style={{borderRadius:'20px'}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/h7oibtpm7kqks8cqz3gh" alt="image" />
-            <div className="card-body">
-                <h5 className="card-title">Waaw Momo</h5>
-                <a href="#" className="btn btn-primary">Explore</a>
-            </div>
-        </div>
-        <div className="card onecard" >
-            <img style={{borderRadius:'20px'}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/h7oibtpm7kqks8cqz3gh" alt="image" />
-            <div className="card-body">
-                <h5 className="card-title">Waaw Momo</h5>
-                <a href="#" className="btn btn-primary">Explore</a>
-            </div>
-        </div>
 
     </div>
     </div>
