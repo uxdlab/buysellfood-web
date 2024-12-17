53c50cf8a0a11b158775e2c5674fb5837a604ef2
viraj-uxd <viraj@uxdlabtech.com>
2024-12-17 13:40:40 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    target: 'es2015',
+  },
+  build: {
+    target: 'es2015',
+  },
 })
